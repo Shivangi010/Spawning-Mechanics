78b1d589656c3ae919e3260201ea818f01641cce
Shivangi Shandilya <53758828+Shivangi010@users.noreply.github.com>
2021-01-07 16:42:13 -0500

    A Reaction Time Game using built-in JavaScript Math library to generate random numbers and shapes.  This game captures the time taken to hit the target that appears on the user screen.
    A Reaction Time Game using built-in JavaScript Math library to generate random numbers and shapes. 
    This game captures the time taken to hit the target that appears on the user screen.

---
+++ script.js
@@ -0,0 +1,100 @@
+bestTime = 0;
+let totalShadow = document.querySelector('.total-shadow');
+
+
+function getRandomColor(){
+    var letters = "0123456789ABCDEF".split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+function makeShapeAppear(){
+    var top = Math.random() * 150;
+    var left = Math.random() * 1100;
+    var width = (Math.random() * 200) + 100;
+
+    
+        if (Math.random() > 0.9) {
+            document.getElementById("shape").style.borderRadius = "50%";
+        } else {
+            document.getElementById("shape").style.borderRadius = "0";
+        }
+
+        document.getElementById("shape").style.backgroundColor = getRandomColor();
+        document.getElementById("shape").style.width = width + "px";
+        document.getElementById("shape").style.height = width + "px";
+        document.getElementById("shape").style.top = top + "px";
+        document.getElementById("shape").style.left = left + "px";
+        document.getElementById("shape").style.display = "block";
+    
+}
+
+function appearAfterDelay(){
+    setTimeout(makeShapeAppear, Math.random() * 500);
+}
+
+function resetTime(){
+    bestTime = 0;
+    document.getElementById("timeTaken").innerHTML = '0s';
+}
+
+function startGame(){
+    appearAfterDelay();
+    window.start = new Date().getTime();
+}
+
+document.getElementById("shape").onclick = function(){
+    document.getElementById("shape").style.display = "none";
+
+    var end = new Date().getTime();
+
+    var timeTaken = (end - window.start) / 1000;
+
+    document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+
+    if(timeTaken < 1){
+
+        document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+        document.getElementById("timeTaken").innerHTML = timeTaken + "s \n\n Nice Work !!";
+        document.getElementById("timeTaken").style.color = "green";
+    }
+    else{
+        document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+     document.getElementById("timeTaken").innerHTML = timeTaken + "s \n\n Not Cool !!";
+     document.getElementById("timeTaken").style.color = "red";
+    }
+
+    
+
+    //window.timeBest = (end - start) / 1000;
+    //document.getElementById("timeBest").innerHTML = timeBest + "s";
+
+}
+document.getElementById("shape").onclick = function(){
+    document.getElementById("shape").style.display = "none";
+
+    var end = new Date().getTime();
+
+    var timeTaken = (end - window.start) / 1000;
+
+    document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+
+    if (bestTime == 0) {
+        bestTime = timeTaken;
+    }
+
+    if (timeTaken >= bestTime) {
+        document.getElementById("timeBest").innerHTML = bestTime + "s";
+    } else {
+        bestTime = timeTaken;
+        document.getElementById("timeBest").innerHTML = bestTime + "s";
+    }
+
+    //window.timeBest = (end - start) / 1000;
+    //document.getElementById("timeBest").innerHTML = timeBest + "s";
+
+}
